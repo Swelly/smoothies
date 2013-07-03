92bf8ba212872bb9fdb3ab354b05fae7f2016edf
Phillip Lamplugh <phillip.lamplugh@gmail.com>
2013-07-03 10:31:05 -0400

    first commit

---
+++ smoothies.rb
@@ -0,0 +1,53 @@
+require 'pry'
+
+# Every Morning I make a smoothie with the follow ingredients:
+smoothie_ingredients = {
+  'flax seeds' => '1 tbsp',
+  'chia seeds' => '1 tbsp',
+  'coconut flakes' => '1 tbsp',
+  'spirulina' => '1 tsp',
+  'pumpkin seeds' => '1 tbsp',
+  'oatmeal' => '1/4 cup',
+  'coco powder' => '1 tbsp',
+  'peanut butter' => '1 tbsp',
+  'almonds' => '1/4 cup',
+  'walnuts' => '1/4 cup',
+  'spinach' => '1/4 cup',
+  'brussel sprouts' => '1/4 cup',
+  'asparagus' => '1/4 cup',
+  'kale' => '1/4 cup',
+  'brocoli rabe' => '1/4 cup',
+  'blue berries' => '1/4 cup',
+  'straw berries' => '1/4 cup',
+  'mango' => '1/4 cup'
+}
+
+# Write a function called blend.
+# It should take all the smoothie ingredients (not the measurements) and chop up and mix all the characters
+# and output a mixed string of characters
+
+def blend smoothie_ingredients
+end
+
+# create a class called Blender
+# It should have a method that takes an array of ingredients and returns a mixed string of characters.
+# Give the blender an on and off switch and only allow the blender to function when it's on.
+# put the smoothie ingredients in the blender
+
+class Blender
+end
+
+# create a class Person. A person will have a stomach and allergies
+# Create a method that allows the person to eat and add arrays of food to their stomachs
+# If a food array contains a known allergy reject the food.
+
+class Person
+end
+
+# Create a Person named Chris. Chris is allergic to gluten.
+# Create a Person named Beth. Beth is allergic to scallops.
+# Feed them the following foods
+
+pizza = ["cheese", "gluten", "tomatoes"]
+pan_seared_scallops = ["scallops", "lemons", "pasta", "olive oil"]
+water = ["h", "h", "o"]
